bf14a2c3c115e87da8c10077bdaeace992bb1174
Kostikova Maria <kostikova-m@hotmail.com>
2021-05-05 00:59:31 +0300

    add IShape& Circle

---
+++ LibCalcArea/IShape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace LibCalcArea
+{
+    public interface IShape
+    {
+        double Area { get; }
+
+        void ShowShape();
+    }
+}
---
+++ LibCalcArea/Shapes/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibCalcArea.Shapes
+{
+    public class Circle : IShape
+    {
+        public double Radius { get; set; }
+
+        public double Area => Math.PI * Math.Pow(Radius, 2);
+
+        public Circle(double rad)
+        {
+            Radius = rad;
+        }
+
+        public override string ToString()
+        {
+            return "Радиус круга " + Radius + "\r\nПлощадь круга = " + Area;
+        }
+
+        public void ShowShape()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        
+    }
+}

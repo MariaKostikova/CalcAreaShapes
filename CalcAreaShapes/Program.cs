a6cbf46c072416716c6cb787f2de6b2936509bde
Kostikova Maria <kostikova-m@hotmail.com>
2021-05-04 22:29:13 +0300

    first commit

---
+++ CalcAreaShapes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalcAreaShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

c4d20797e7bf5f8d302c6653e25be0fc39e0c6a0
Kostikova Maria <kostikova-m@hotmail.com>
2021-05-05 13:50:13 +0300

    add ShapeRandom

---
+++ LibCalcArea/Shapes/ShapeRandom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibCalcArea.Shapes
+{
+    class ShapeRandom : IShape
+    {
+        private IShape shape;
+
+        public double Area => shape.Area;
+        
+        public ShapeRandom(IShape _shape)
+        {
+            shape = _shape;
+        }
+
+        public override string ToString()
+        {
+            return "Площадь фигуры = " + Area;
+        }
+
+        public void ShowShape()
+        {
+            Console.WriteLine(ToString());
+        }
+
+
+
+    }
+}
